dcfe625370b23e73b19c74ff2fbc6b3a0a7e6137
ariel <ariel10player@gmail.com>
2024-10-25 19:08:04 -0300

    imagenes cambio

--- index.js
+++ index.js
@@ -12,110 +12,224 @@ let nav = document.querySelectorAll("body header nav ul li a")
 let ul = document.querySelector("body header nav ul")
 let li = document.querySelector("body header nav ul li")
 let carrito = document.querySelector("body header .iconos-boton .carrito .fa-cart-plus")
-let boton_cart =  document.querySelector("body header .iconos-boton .carrito button")
-
-burguer.addEventListener("click",()=>{
+let boton_cart = document.querySelector("body header .iconos-boton .carrito button")
+let imagen2 = document.querySelector("body main .imagenes .imagen2")
+let imagen1 = document.querySelector("body main .imagenes .imagen1")
+burguer.addEventListener("click", () => {
     console.log("locura total")
-    header.style.left = "0%"; 
+    header.style.left = "0%";
 })
-x.addEventListener("click", ()=>{
-    header.style.left="-100%";   
+x.addEventListener("click", () => {
+    header.style.left = "-100%";
 })
 
-boton_oscuro.addEventListener("click",(event)=>{
+boton_oscuro.addEventListener("click", (event) => {
     console.log("locura total y absoluta")
-    if (body.style.backgroundColor=="white"){
-        body.style.backgroundColor="black";
-    }else{
-        body.style.backgroundColor="white";
+
+    if (imagen1.style.display == "inline") {
+        imagen1.style.display = "none";
+    } else {
+        imagen1.style.display = "inline";
+        imagen2.style.left = "-100%";
     }
-    if (main.style.backgroundColor=="white"){
-        main.style.backgroundColor="black";
-    }else{
-        main.style.backgroundColor="white";
-    }
-    if (header.style.backgroundColor=="white"){
-        header.style.backgroundColor="black";
-    }else{
-        header.style.backgroundColor="white";
-    }
-    if (x.style.color=="black"){
-        x.style.color="white";
-    }else{
-        x.style.color="black";
-    }
-    if (boton_oscuro.style.backgroundColor=="black"){
-        boton_oscuro.style.backgroundColor="white";
-    }else{
-        boton_oscuro.style.backgroundColor="black";
-    }
-    if (boton_oscuro.style.color=="white"){
-        boton_oscuro.style.color="black";
-    }else{
-        boton_oscuro.style.color="white";
+    if (imagen2.style.left == "-100%") {
+        imagen2.style.left = "0%";
+        imagen2.style.display = "inline";
+
+    } else {
+        imagen2.style.left = "-100%";
+        imagen1.style.display = "inline";
     }
 
-    if (titulo.style.backgroundColor=="white"){
-        titulo.style.backgroundColor="black";
-    }else{
-        titulo.style.backgroundColor="white";
+
+    if (body.style.backgroundColor == "white") {
+        body.style.backgroundColor = "black";
+    } else {
+        body.style.backgroundColor = "white";
     }
-    if (titulo.style.color=="black"){
-        titulo.style.color="white";
-    }else{
-        titulo.style.color="black";
+    if (main.style.backgroundColor == "white") {
+        main.style.backgroundColor = "black";
+    } else {
+        main.style.backgroundColor = "white";
     }
-    if (logo.style.color=="black"){
-        logo.style.color="white";
-    }else{
-        logo.style.color="black";
+    if (header.style.backgroundColor == "white") {
+        header.style.backgroundColor = "black";
+    } else {
+        header.style.backgroundColor = "white";
     }
-    
-    for (let i = 0; i < nav.length; i++){
-        if (nav[i].style.color=="black"){
-            nav[i].style.color="white";
-        }else{
-            nav[i].style.color="black";
+    if (x.style.color == "black") {
+        x.style.color = "white";
+    } else {
+        x.style.color = "black";
+    }
+    if (boton_oscuro.style.backgroundColor == "black") {
+        boton_oscuro.style.backgroundColor = "white";
+    } else {
+        boton_oscuro.style.backgroundColor = "black";
+    }
+    if (boton_oscuro.style.color == "white") {
+        boton_oscuro.style.color = "black";
+    } else {
+        boton_oscuro.style.color = "white";
+    }
+
+    if (titulo.style.backgroundColor == "white") {
+        titulo.style.backgroundColor = "black";
+    } else {
+        titulo.style.backgroundColor = "white";
+    }
+    if (titulo.style.color == "black") {
+        titulo.style.color = "white";
+    } else {
+        titulo.style.color = "black";
+    }
+    if (logo.style.color == "black") {
+        logo.style.color = "white";
+    } else {
+        logo.style.color = "black";
+    }
+
+    for (let i = 0; i < nav.length; i++) {
+        if (nav[i].style.color == "black") {
+            nav[i].style.color = "white";
+        } else {
+            nav[i].style.color = "black";
         }
     }
-    for (let i = 0; i < carrito.length; i++){
-        carrito[i].style.border= "10px solid white";
-    }
-   
-    if (ul.style.color=="black"){
-        ul.style.color="white";
-    }else{
-        ul.style.color="black";
-    }   
-    
-    
-    if (idioma.style.color=="black"){
-        idioma.style.color="white";
-    }else{
-        idioma.style.color="black";
-    }
-    if (carrito.style.color=="black"){
-        carrito.style.color="white";
-    }else{
-        carrito.style.color="black";
+    for (let i = 0; i < carrito.length; i++) {
+        carrito[i].style.border = "10px solid white";
     }
 
-    
-    if (boton_cart.style.backgroundColor == "black"){
-        boton_cart.style.backgroundColor = "white";
-    }else{
-        boton_cart.style.backgroundColor="black";
+    if (ul.style.color == "black") {
+        ul.style.color = "white";
+    } else {
+        ul.style.color = "black";
     }
-    
-    if (boton_cart.style.color =="white"){
+
+
+    if (idioma.style.color == "black") {
+        idioma.style.color = "white";
+    } else {
+        idioma.style.color = "black";
+    }
+    if (carrito.style.color == "black") {
+        carrito.style.color = "white";
+    } else {
+        carrito.style.color = "black";
+    }
+
+
+    if (boton_cart.style.backgroundColor == "black") {
+        boton_cart.style.backgroundColor = "white";
+    } else {
+        boton_cart.style.backgroundColor = "black";
+    }
+
+    if (boton_cart.style.color == "white") {
         boton_cart.style.color = "black";
-    }else{
+    } else {
         boton_cart.style.color = "white";
     }
-    if (burguer.style.color=="black"){
-        burguer.style.color="white";
-    }else{
-        burguer.style.color="black";
-    }   
+    if (burguer.style.color == "black") {
+        burguer.style.color = "white";
+    } else {
+        burguer.style.color = "black";
+    }
 
-})
+}
+)
+   
+if (body.style.backgroundColor == "white") {
+    body.style.backgroundColor = "black";
+} else {
+    body.style.backgroundColor = "white";
+}
+if (main.style.backgroundColor == "white") {
+    main.style.backgroundColor = "black";
+} else {
+    main.style.backgroundColor = "white";
+}
+if (header.style.backgroundColor == "white") {
+    header.style.backgroundColor = "black";
+} else {
+    header.style.backgroundColor = "white";
+}
+if (x.style.color == "black") {
+    x.style.color = "white";
+} else {
+    x.style.color = "black";
+}
+if (boton_oscuro.style.backgroundColor == "black") {
+    boton_oscuro.style.backgroundColor = "white";
+} else {
+    boton_oscuro.style.backgroundColor = "black";
+}
+if (boton_oscuro.style.color == "white") {
+    boton_oscuro.style.color = "black";
+} else {
+    boton_oscuro.style.color = "white";
+}
+
+if (titulo.style.backgroundColor == "white") {
+    titulo.style.backgroundColor = "black";
+} else {
+    titulo.style.backgroundColor = "white";
+}
+if (titulo.style.color == "black") {
+    titulo.style.color = "white";
+} else {
+    titulo.style.color = "black";
+}
+if (logo.style.color == "black") {
+    logo.style.color = "white";
+} else {
+    logo.style.color = "black";
+}
+
+for (let i = 0; i < nav.length; i++) {
+    if (nav[i].style.color == "black") {
+        nav[i].style.color = "white";
+    } else {
+        nav[i].style.color = "black";
+    }
+}
+for (let i = 0; i < carrito.length; i++) {
+    carrito[i].style.border = "10px solid white";
+}
+
+if (ul.style.color == "black") {
+    ul.style.color = "white";
+} else {
+    ul.style.color = "black";
+}
+
+
+if (idioma.style.color == "black") {
+    idioma.style.color = "white";
+} else {
+    idioma.style.color = "black";
+}
+if (carrito.style.color == "black") {
+    carrito.style.color = "white";
+} else {
+    carrito.style.color = "black";
+}
+
+
+if (boton_cart.style.backgroundColor == "black") {
+    boton_cart.style.backgroundColor = "white";
+} else {
+    boton_cart.style.backgroundColor = "black";
+}
+
+if (boton_cart.style.color == "white") {
+    boton_cart.style.color = "black";
+} else {
+    boton_cart.style.color = "white";
+}
+if (burguer.style.color == "black") {
+    burguer.style.color = "white";
+} else {
+    burguer.style.color = "black";
+}
+
